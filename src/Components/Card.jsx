805cf6a07158ec85fb1552bf6b4d0ee1fa80ab79
vishroy hangar <vishroy.seenarain@hangarww.com>
2021-11-20 07:39:40 +0400

    Resolved array.map for CardList and cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import '../sass/Card.scss';
 
 
-const Card = ({link, variation, image, title, text}) => {
+const Card = props => {
     return (
-        <a className="card__link" href={link}>
-            <div className={`card ${variation}`}>
-                <div className={`card__image ${variation === 'card--small' ? 'card__image--small' : ''}`}>
-                    <img src={image} />
+        <a className="card__link" href={props.link}>
+            <div className={`card ${props.variation}`}>
+                <div className={`card__image ${props.variation === 'card--small' ? 'card__image--small' : ''}`}>
+                    <img src={props.image} />
                 </div>
 
                 <div className="card__text-content">
                     <h3>
-                        {title} <span className="material-icons">arrow_forward</span>
+                        {props.title} <span className="material-icons">arrow_forward</span>
                     </h3>
 
                     <p>
-                        {text}
+                        {props.text}
                     </p>
                 </div>
             </div>
--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -10,58 +10,47 @@ class CardList extends React.Component {
         this.state = {
             content: [
                 {
-                    id: '',
+                    id: '01',
                     link: 'https://www.google.com',
+                    variation: '',
                     image: 'https://www.electrifyamerica.com/img/member.3601bd05.png',
                     title: 'BECOME A MEMBER',
                     text: 'Need to fill up on the road? Enjoy discounted pricing with our Pass+ monthly subscription plan.'
+                },
+                {
+                    id: '02',
+                    link: 'https://www.google.com',
+                    variation: 'card--small',
+                    image: 'https://www.electrifyamerica.com/img/home_green_city.dc7925a8.png',
+                    title: 'OUR GREEN CITY',
+                    text: 'Learn how we’re accelerating freight and transit electrification in Southern California’s most bustling port locations.'
+                },
+                {
+                    id: '03',
+                    link: 'https://www.google.com',
+                    variation: '',
+                    image: 'https://www.electrifyamerica.com/img/mobile_app_desktop.1821098b.png',
+                    title: 'MOBILE APP',
+                    text: 'Find stations, start charging, track and end your session from your smartphone with our app for iOS and Android.'
+                },
+                {
+                    id: '04',
+                    link: 'https://www.google.com',
+                    variation: 'card--small',
+                    image: 'https://www.electrifyamerica.com/img/home_charger_HP.7e321604.png',
+                    title: 'HOME PRODUCTS',
+                    text: 'Simplify life with HomeStationTM. Our Level 2 EV home charger offers remote start, stop, and scheduling via the Electrify America app for a streamlined charging experience.'
+                },
+                {
+                    id: '05',
+                    link: 'https://www.google.com',
+                    variation: '',
+                    image: 'https://www.electrifyamerica.com/img/support_desktop.26504b28.png',
+                    title: 'SUPPORT & TROUBLESHOOTING',
+                    text: 'Have a question or issue with a charging station? Our team is here to help.Call us at 1-833 - 632 - 2778 or use our contact form.'
                 }
             ]
         }
-
-        // card contents as an array
-        this.content = [
-            {
-                id: '',
-                link: 'https://www.google.com',
-                variation: '',
-                image: 'https://www.electrifyamerica.com/img/member.3601bd05.png',
-                title: 'BECOME A MEMBER',
-                text: 'Need to fill up on the road? Enjoy discounted pricing with our Pass+ monthly subscription plan.'
-            },
-            {
-                id: '',
-                link: 'https://www.google.com',
-                variation: 'card--small',
-                image: 'https://www.electrifyamerica.com/img/home_green_city.dc7925a8.png',
-                title: 'OUR GREEN CITY',
-                text: 'Learn how we’re accelerating freight and transit electrification in Southern California’s most bustling port locations.'
-            },
-            {
-                id: '',
-                link: 'https://www.google.com',
-                variation: '',
-                image: 'https://www.electrifyamerica.com/img/mobile_app_desktop.1821098b.png',
-                title: 'MOBILE APP',
-                text: 'Find stations, start charging, track and end your session from your smartphone with our app for iOS and Android.'
-            },
-            {
-                id: '',
-                link: 'https://www.google.com',
-                variation: 'card--small',
-                image: 'https://www.electrifyamerica.com/img/home_charger_HP.7e321604.png',
-                title: 'HOME PRODUCTS',
-                text: 'Simplify life with HomeStationTM. Our Level 2 EV home charger offers remote start, stop, and scheduling via the Electrify America app for a streamlined charging experience.'
-            },
-            {
-                id: '',
-                link: 'https://www.google.com',
-                variation: '',
-                image: 'https://www.electrifyamerica.com/img/support_desktop.26504b28.png',
-                title: 'SUPPORT & TROUBLESHOOTING',
-                text: 'Have a question or issue with a charging station? Our team is here to help.Call us at 1-833 - 632 - 2778 or use our contact form.'
-            }
-        ]
     }
 
 
@@ -73,18 +62,19 @@ class CardList extends React.Component {
     render() {
         return (
             <div className="card-list-container">
-                <h2>
-                    Learn about charging with
+                <h2 className='card-list-container__title'>
+                    Learn about charging with <br />
                     Electrify America
                 </h2>
-                <div className="card-list">
-                    {/* {this.content.map(item => {
-                    <Card id={item.id} link={item.link} variation={item.variation} />
-                })} */}
 
-                    <Card link={this.content[0].link} variation={''} image={this.content[0].image} title={this.content[0].title} text={this.content[0].text} />
-                    <Card link={this.content[1].link} variation={'card--small'} image={this.content[1].image} title={this.content[1].title} text={this.content[1].text} />
-                    <Card link={this.content[2].link} variation={''} image={this.content[2].image} title={this.content[2].title} text={this.content[2].text} />
+                <div className="card-list">
+                    {
+                        this.state.content.map(item => (
+                            <Card key={item.id} link={item.link} variation={item.variation} image={item.image} title={item.title} text={item.text}/>
+                        ))
+                    }
+
+                    
                 </div>
             </div>
         )
