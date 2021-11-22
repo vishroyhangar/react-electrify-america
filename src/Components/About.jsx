8c7640c4ad39e2845a87958a2923c9b9827c321e
vishroy hangar <vishroy.seenarain@hangarww.com>
2021-11-22 04:12:22 +0400

    basic about section done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,10 +3,23 @@ import '../sass/About.scss';
 
 export default function About() {
     return (
-        <div className="about-content">
-            <div className="about_content__details">
-                Hello
-            </div>
-        </div>
+        <section className="section-about">
+            <div className="section-about__details header__details">
+                <a href="/">
+                    <h3>
+                        ABOUT US
+                        <span className="material-icons">arrow_forward</span>
+                    </h3>
+                </a>
+
+                <h1>
+                    We're leading the charge of fast charging
+                </h1>
+
+                <p>
+                    We’re building out a convenient, reliable, customer-centric network of electric vehicle chargers nationwide—at workplaces, in communities, and on highways.
+                </p>
+            </div>  
+        </section>
     )
 }
