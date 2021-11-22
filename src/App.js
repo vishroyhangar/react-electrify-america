33f51e7a299dc2b4a20005061a979141560342aa
vishroy hangar <vishroy.seenarain@hangarww.com>
2021-11-22 10:01:32 +0400

    Resolving log errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import CardList from './Components/CardList'
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Main from './Components/Main';
